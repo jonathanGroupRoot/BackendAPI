a0b7512cb1741080a40a3e430d6a5d36ef574c1a
Your Name <you@example.com>
2020-01-01 21:52:37 -0300

    Preparing FrontEnd

---
+++ frontend/src/components/menu/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Menu extends React.Component{
+    render(){
+        return(
+            <h1>
+                Uma bela barra de menu
+            </h1>
+        );
+    }
+}
