666d9e3a3ced30eca3a0327b91d06dc98e491c95
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-01-17 18:35:40 -0300

    Atualizando Controller\Migrations

--- app/Http/Controllers/CaixaController.php
+++ app/Http/Controllers/CaixaController.php
@@ -31,9 +31,11 @@ class CaixaController extends Controller
         $caixa->id_Colaborador = $request->colaborador;
         $caixa->id_Consulta = $request->consulta;
         foreach($caixa as $tot)
-        if($caixa->tipoDeEntrada === 1)
+        if($tot->tipoDeEntrada === 1)
         {
             $tot += $caixa->valor;
+        }else{
+            $tot -= $caixa->valor;
         }
         $caixa->save();
         
