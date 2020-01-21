b66889ee269e9743434b0cab5159b5a57172b14f
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-01-20 21:18:06 -0300

    Autenticando Validações

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -28,8 +28,8 @@ class ClienteController extends Controller
             'sexo' => 'required',
             'nacionalidade' => 'required'
         ],[
-            "nome.min" => ["No Mínimo 5 Caracteres"],
-            "nome.max" => ["No Máximo 255 Caracteres"],
+            "nome.min" => $this->json('No Minimo 5 caracteres'),
+            "nome.max" => "No Máximo 255 Caracteres",
         ]);
         $pessoa = new Pessoa();
         $pessoa->nome = $request->nome;
