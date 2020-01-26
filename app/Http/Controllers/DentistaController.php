ac200dcec7e53b6594cc08b873cb2d300d03163c
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-01-25 23:08:51 -0300

    Atualizando Validatores

--- app/Http/Controllers/DentistaController.php
+++ app/Http/Controllers/DentistaController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use Illuminate\Validation\ValidationException
+use Illuminate\Validation\ValidationException;
 use App\Dentista;
 use App\Pessoa;
 use App\Colaborador;
