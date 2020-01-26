683864d335947d8e463d0cc792d67149c17da831
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-01-25 22:45:09 -0300

    Atualizando Validatores

--- app/Http/Controllers/DentistaController.php
+++ app/Http/Controllers/DentistaController.php
@@ -40,77 +40,43 @@ class DentistaController extends Controller
         ],$messages);
        if ($validator->fails())
        {
+           $msg = "Algum Dado Está Incorreto";
            $error = $validator->errors();
        }
-        // ],[
-            
-        //     'nome.min' => 'No Mínimo 5 Caracteres!!',
-        //     'nome.max' => 'No Máximo 255 Caracteres!!',
-        //     "CPF.min" => ('No Mínimo 14 Caracteres!!'),
-        //     "CPF.max" => ('No Máximo 14 Caracteres!!'),
-        //     "CPF.unique" => ('O CPF Digitado já existe no sistema verifique e tente novamente!!'),
-        //     "CEP.min" => ('No Mínimo 9 Caracteres!!'),
-        //     "CEP.max" => ('No Máximo 9 Caracteres!!'),
-        //     "dataDeNascimento.required" => ('Campo Obrigatório!!'),
-        //     "RG.min" => ('No Mínimo 7 Caracteres!!'),
-        //     "RG.max" => ('No Máximo 7 Caracteres!!'),
-        //     "RG.unique" =>('O RG digitado já existe no sistema verifique e tente novamente!!'),
-        //     "endereco.min" => ('No Mínimo 5 Caracteres!!'),
-        //     "endereco.max" => ('No Máximo 255 Caracteres!!'),
-        //     "telefone.min" => ('No Mínimo 16 Caracteres!!'),
-        //     "telefone.max" => ('No Máximo 16 Caracteres!!'),
-        //     "sexo.required" => ('O campo sexo e Obrigatorio!!'),
-        //     "nacionalidade.required" => ('O campo nacionalidade e Obrigatorio!!'),
-        //     "PIS.min" => ('Mínimo 14 Caracteres!!'),
-        //     "PIS.max" => ('Máximo 14 Caracteres!!'),
-        //     "PIS.required" => ('O campo é obrogatório'),
-        //     "cargo.min" => ('Mínimo 5 Caracteres!!'),
-        //     "cargo.max" => ('Máximo 255 Caracteres!!'),
-        //     "conta.min" => ('Mínimo 10 Caracteres!!'),
-        //     "conta.max" => ('Máximo 10 Caracteres!!'),
-        //     "conta.required" => ('O campo é Obrigatório!!'),
-        //     "tipoDaConta.min" => ('Mínimo 5 Caracteres!!'),
-        //     "tipoDaConta.max" => ('Máximo 5 Caracteres!!'),
-        //     "tipoDaConta.required" => ('O Campo é Obrigatório!!'),
-        //     "agencia.min" => ('Mínimo 4 Caracteres!!'),
-        //     "agencia.max" => ('Máximo 4 Caracteres!!'),
-        //     "agencia.required" => ('O Campo é Obrigatório!!'),
-        //     "salario.required" => ('O Campo é Obrigatório!!'),
-        //     "dataDeAdmissão.required" => ('O Campo é Obrigatório!!')
-        // ]);
- 
-    $pessoa = new Pessoa();
-    $pessoa->nome = $request->nome;
-    $pessoa->CPF = $request->CPF;
-    $pessoa->CEP = $request->CEP;
-    $pessoa->dataDeNascimento = $request->dataDeNascimento;
-    $pessoa->RG = $request->RG;
-    $pessoa->endereco = $request->endereco;
-    $pessoa->telefone = $request->telefone;
-    $pessoa->sexo = $request->sexo;
-    $pessoa->nacionalidade = $request->nacionalidade;
-    $pessoa->ativo = $request->ativo;
-    $pessoa->save();
-
-    $colaborador = new Colaborador();
-    $colaborador->PIS = $request->PIS;
-    $colaborador->cargo = $request->cargo;
-    $colaborador->conta = $request->conta;
-    $colaborador->tipoDaConta = $request->tipoDaConta;
-    $colaborador->agencia = $request->agencia;
-    $colaborador->salario = $request->salario;
-    $colaborador->dataDeAdmissao = $request->dataDeAdmissao;
-    $colaborador->Pessoa_idPessoa = $pessoa->id;
-    $colaborador->save();
-
-    $dentista = new Dentista();
-    $dentista->CRO = $request->CRO;
-    $dentista->especialidades = $request->especialidades;
-    $dentista->responsavelTecnico = $request->responsavelTecnico;
-    $dentista->Colaborador_idColaborador = $colaborador->id;
-    $dentista->save();
-    return response()->json('Cadastrado Com Sucesso!!!');
-
+    else{
+            $pessoa = new Pessoa();
+            $pessoa->nome = $request->nome;
+            $pessoa->CPF = $request->CPF;
+            $pessoa->CEP = $request->CEP;
+            $pessoa->dataDeNascimento = $request->dataDeNascimento;
+            $pessoa->RG = $request->RG;
+            $pessoa->endereco = $request->endereco;
+            $pessoa->telefone = $request->telefone;
+            $pessoa->sexo = $request->sexo;
+            $pessoa->nacionalidade = $request->nacionalidade;
+            $pessoa->ativo = $request->ativo;
+            $pessoa->save();
+        
+            $colaborador = new Colaborador();
+            $colaborador->PIS = $request->PIS;
+            $colaborador->cargo = $request->cargo;
+            $colaborador->conta = $request->conta;
+            $colaborador->tipoDaConta = $request->tipoDaConta;
+            $colaborador->agencia = $request->agencia;
+            $colaborador->salario = $request->salario;
+            $colaborador->dataDeAdmissao = $request->dataDeAdmissao;
+            $colaborador->Pessoa_idPessoa = $pessoa->id;
+            $colaborador->save();
+        
+            $dentista = new Dentista();
+            $dentista->CRO = $request->CRO;
+            $dentista->especialidades = $request->especialidades;
+            $dentista->responsavelTecnico = $request->responsavelTecnico;
+            $dentista->Colaborador_idColaborador = $colaborador->id;
+            $dentista->save();
+            return response()->json('Cadastrado Com Sucesso!!!');
+        }
+    
     }
     public function deleteDentista($id)
     {
