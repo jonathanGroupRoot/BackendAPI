b75678833e36db7b7e53f2e0d21098652aaaa795
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-01-29 17:47:32 -0300

    Inserindo JWT, Providers JWT

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\RequestGuard;
 use Tymon\JWTAuth\JWTAuth;
 use App\Usuario;
 
