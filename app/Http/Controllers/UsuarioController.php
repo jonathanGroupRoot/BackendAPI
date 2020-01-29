3296c42b1f27d8ad9dc972f8229cef55969b9357
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-01-29 17:42:28 -0300

    Inserindo JWT, Providers JWT

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -34,7 +34,7 @@ class UsuarioController extends Controller
             'usuario' => 'required|min:5|max:40|unique:usuarios,usuario',
             'password' => 'required|min:8:max:40',
         ],$messages);
-        if(! $token = $this->jwt->claims(['usuario' => $request->usuario])->attempt($request->only('usuario','password')))
+        if(! $token = $this->jwt->attempt($request->only('usuario','password')))
         {
             return response()->json(['Usuário não encontrado'],404);
         }
