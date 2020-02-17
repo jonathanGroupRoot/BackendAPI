7cfcdffdcbb7a6d920430ffc4e3e3666e421131e
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-02-17 20:53:20 -0300

    Inserindo Pesquisa Inputs

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -103,7 +103,7 @@ class ClienteController extends Controller
     }
     public function pesquisarClientes()
     {
-        $produtos = Pessoa::when(Request::input('nome'),function($query){
+        $produtos = Pessoa::all(Request::input('nome'),function($query){
             $query->where('nome',Request::input('nome'));
         })->get();
     }
