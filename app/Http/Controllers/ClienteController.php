29a1b147ee674238d5b674bca9465a10e69cbd03
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-02-18 20:18:23 -0300

    Atualizando Controller

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -113,11 +113,11 @@ class ClienteController extends Controller
     }
     public function pesquisarClientes(Request $request)
     {
-     
+        $nome = $request->only('nome');
         $dados = DB::table('pessoas')
         ->join('clientes','clientes.Pessoa_idPessoa', '=', 'pessoas.id')
         ->select('pessoas.*')
-        ->where('pessoas.nome', 'LIKE', '%'.$request)
+        ->where('pessoas.nome', 'LIKE', '%'.$nome,'%')
         ->get();
         return response()->json($dados);
     }
