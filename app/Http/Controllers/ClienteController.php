b379df6428c8282598d10c1d5b50b0e35d99d805
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-02-18 19:38:14 -0300

    Atualizando Controller

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -116,7 +116,7 @@ class ClienteController extends Controller
         $nome = $request->get('nome');
         $dados = DB::table('pessoas')
         ->join('clientes','clientes.Pessoa_idPessoa', '=', 'pessoas.id')
-        ->select('pessoas*')
+        ->select('pessoas.*')
         ->where('nome', 'LIKE', '%'.$nome.'%')->get();
         return response()->json($dados);
     }
