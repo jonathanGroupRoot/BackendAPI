0f766a9721ab8ef153b975fa563efd4850c3a084
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-02-18 18:57:37 -0300

    Atualizando Controller

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -100,7 +100,7 @@ class ClienteController extends Controller
         'pessoas.CPF','pessoas.RG','pessoas.endereco',
         'pessoas.CEP','pessoas.telefone','pessoas.sexo','pessoas.nacionalidade',
         'pessoas.motivo','pessoas.ativo')
-        ->where('clientes->id === $id')
+        ->where('clientes.id === $id')
         ->get();
         
         return response()->json($cliente);
