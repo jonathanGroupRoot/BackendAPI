9cdef0eb858d07f1fd7d46be57d5f1b4a688b4a8
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-02-19 19:34:43 -0300

    Atualizando Controllers

--- app/Http/Controllers/DentistaController.php
+++ app/Http/Controllers/DentistaController.php
@@ -15,7 +15,7 @@ class DentistaController extends Controller
     {
         $dentista = DB::table('colaboradors')
         ->join('dentistas','dentistas.Colaborador_idColaborador', '=', 'colaboradors.id')
-        ->select('dentistas.*','colaboradors.*')->get();
+        ->select('colaboradors.*','dentistas.*')->get();
         return response()->json($dentista);
     }
     public function cadastrarDentista(Request $request)
