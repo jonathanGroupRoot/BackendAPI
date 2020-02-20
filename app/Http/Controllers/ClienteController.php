5462952609bd5c86de939780517db8a91c5b539f
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-02-20 18:58:31 -0300

    Atualizando Listagem Controllers

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -141,8 +141,7 @@ class ClienteController extends Controller
         ],$messages);
         $registros = $request->all();
         $clientes = Cliente::find($id);
-        foreach($clientes as $cliente)
-        Pessoa::find($cliente->Pessoa_idPessoa)->update($registros);
+        Pessoa::find($Pessoa_idPessoa)->update($registros);
         return response()->json('Cliente Atualizado Com Sucesso!!');
     }
     public function pesquisarClientes(Request $request)
