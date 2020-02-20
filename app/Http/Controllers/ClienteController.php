d4869e3c9a382bb5aaf829417dbf956f598e14cf
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-02-20 19:19:11 -0300

    Atualizando Listagem Controllers

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -128,11 +128,11 @@ class ClienteController extends Controller
         ];
         $this->validate($request,[
             'nome' => 'required|min:5|max:255',
-            'CPF' => 'required|min:14|max:14'.'Pessoa_idPessoa'.$id,
+            'CPF' => 'required|min:14|max:14'.'pessoas'.$id,
             'CPF.*.first_name' => 'required_with:CPF.*.last_name',
             'CEP' => 'required|min:9|max:9',
             'dataDeNascimento' => 'required|date',
-            'RG' => 'required|min:7|max:7'.'Pessoa_idPessoa'.$id,
+            'RG' => 'required|min:7|max:7'.'pessoas'.$id,
             'RG.*.first_name' => 'required_with:RG.*.last_name',
             'endereco' => 'required|min:5|max:255',
             'telefone' => 'required|min:16|max:16',
@@ -141,7 +141,7 @@ class ClienteController extends Controller
         ],$messages);
         $registros = $request->all();
         $clientes = Cliente::find($id);
-        Pessoa::find($Pessoa_idPessoa)->update($registros);
+        Pessoa::find($clientes->Pessoa_idPessoa)->update($registros);
         return response()->json('Cliente Atualizado Com Sucesso!!');
     }
     public function pesquisarClientes(Request $request)
