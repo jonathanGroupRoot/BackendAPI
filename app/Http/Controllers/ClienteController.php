2c541c13c8871adf2c9d028c5cbf82b804dd750f
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-02-20 17:51:45 -0300

    Atualizando Listagem Controllers

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -128,11 +128,11 @@ class ClienteController extends Controller
         ];
         $this->validate($request,[
             'nome' => 'required|min:5|max:255',
-            'CPF' => 'required|min:14|max:14|bail|unique:pessoas,CPF'.$this->CPF->id,
+            'CPF' => 'required|min:14|max:14|bail|unique:pessoas,CPF|'.Rule::unique('clientes')->ignore($CPF->id, 'Pessoa_idPessoa'),
             'CPF.*.first_name' => 'required_with:CPF.*.last_name',
             'CEP' => 'required|min:9|max:9',
             'dataDeNascimento' => 'required|date',
-            'RG' => 'required|min:7|max:7|bail|unique:pessoas,RG'.$this->CPF->id,
+            'RG' => 'required|min:7|max:7|bail|unique:pessoas,RG'.Rule::unique('clientes')->ignore($RG->id, 'Pessoa_idPessoa'),
             'RG.*.first_name' => 'required_with:RG.*.last_name',
             'endereco' => 'required|min:5|max:255',
             'telefone' => 'required|min:16|max:16',
