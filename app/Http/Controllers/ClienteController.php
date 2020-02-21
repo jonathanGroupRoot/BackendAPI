dd0dd28d6728b402991b1562f678141c73f3db0d
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-02-20 22:51:57 -0300

    Atualizando Listagem Controllers

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -131,26 +131,26 @@ class ClienteController extends Controller
         //     'nacionalidade.required' => 'Este campo é obrigatório',
         // ];
 
-        $validate = Validator::make($request, [
-            'CPF' => [
-                'required',
-                Rule::unique('clientes')->ignore($request->Pessoa_idPessoa),
-            ],
-        ]);
+        // $validate = Validator::make($request, [
+        //     'CPF' => [
+        //         'required',
+        //         Rule::unique('clientes')->ignore($request->Pessoa_idPessoa),
+        //     ],
+        // ]);
         
-        // $this->validate($request,[
-        //     'nome' => 'required|min:5|max:255',
-        //     'CPF' => "required|min:14|max:14|bail".'id'.$id,
-        //     'CPF.*.first_name' => 'required_with:CPF.*.last_name',
-        //     'CEP' => 'required|min:9|max:9',
-        //     'dataDeNascimento' => 'required|date',
-        //     "RG' => 'required|min:7|max:7|bail".'id'.$id,
-        //     'RG.*.first_name' => 'required_with:RG.*.last_name',
-        //     'endereco' => 'required|min:5|max:255',
-        //     'telefone' => 'required|min:16|max:16',
-        //     'sexo' => 'required|boolean',
-        //     'nacionalidade' => 'required',
-        // ],$messages);
+        $this->validate($request,[
+          'nome' => 'required|min:5|max:255',
+            'CPF' => "required|min:14|max:14|bail|unique:pessoas,CPF",
+            'CPF.*.first_name' => 'required_with:CPF.*.last_name',
+            'CEP' => 'required|min:9|max:9',
+            'dataDeNascimento' => 'required|date',
+            "RG' => 'required|min:7|max:7|bail|unique:pessoas,RG",
+            'RG.*.first_name' => 'required_with:RG.*.last_name',
+            'endereco' => 'required|min:5|max:255',
+            'telefone' => 'required|min:16|max:16',
+            'sexo' => 'required|boolean',
+            'nacionalidade' => 'required',
+        ],$messages);
         $registros = $request->all();
         $clientes = Cliente::find($id);
         Pessoa::find($clientes->Pessoa_idPessoa)->update($registros);
