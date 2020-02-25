d582b2f9e88af776e55aa4d89dce3d81dabc3d30
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-02-24 22:46:37 -0300

    Atualizando Listagem Controllers

--- app/Http/Controllers/DentistaController.php
+++ app/Http/Controllers/DentistaController.php
@@ -225,11 +225,11 @@ class DentistaController extends Controller
                 'responsavelTecnico' => 'required|boolean',
                
             ],$messages);
-        $pessoa = new Pessoa();
         $registros = $request->all();
         $dentistas = Dentista::find($id);
-        Colaborador::find($dentistas->Colaborador_idColaborador)->update($registros);
-        Pessoa::find($dentistas->$pessoa->id);
+        $up = Colaborador::find($dentistas->Colaborador_idColaborador)->update($registros);
+        Pessoa::find($up->Colaborador_idColaborador)->update($registros);
+        
 
         return response()->json('Atualizado Com Sucesso!!');
     }
