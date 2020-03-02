38023acaf6ce727646a6cd0f57eb565f61d066fc
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-03-02 19:04:23 -0300

    Atualizando Listagem Controllers

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -74,17 +74,8 @@ class ClienteController extends Controller
         $cliente = new Cliente();
         $cliente->Pessoa_idPessoa = $pessoa->id;
         $cliente->Acompanhante_idAcompanhante = $request->Acompanhante_idAcompanhante; 
-        if($cliente->Acompanhante_idAcompanhante == '')
-        {
-            $cliente->Acompanhante_idAcompanhante = '';
-        }else{
-            $cliente->Acompanhante_idAcompanhante = $request->Acompanhante_idAcompanhante;
-        }
         return response()->json('Cliente Cadastrado Com Sucesso!!');
         $cliente->save();
-       
-        
-        
 
     }
     public function listarCliente()
