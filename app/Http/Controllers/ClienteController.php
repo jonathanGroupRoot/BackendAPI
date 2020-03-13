a5f9343e6e0e3a1de9119914fa38eac996254a9f
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-03-13 16:14:26 -0300

    Atualizando Listagem Controllers

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -142,7 +142,6 @@ class ClienteController extends Controller
         // $acompanhante->update($registros);
         $atualizar = DB::table('pessoas')
         ->join('clientes', 'clientes.Pessoa_idPessoa','=','pessoas.id')
-        ->join('acompanhantes','acompanhantes.Pessoa_idPessoa','=','pessoas.id')
         ->update($registros);
         return response()->json('Cliente Atualizado Com Sucesso!!');
     }
