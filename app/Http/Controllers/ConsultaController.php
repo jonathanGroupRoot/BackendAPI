ceee749c75db5843f1b3a025d973f3ec4f6c3a6b
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-03-19 18:34:41 -0300

    Atualizando Listagem Controllers

--- app/Http/Controllers/ConsultaController.php
+++ app/Http/Controllers/ConsultaController.php
@@ -15,7 +15,7 @@ class ConsultaController extends Controller
     {
         $consulta = DB::table('consultas')
         ->join('procedimentos','consultas.Procedimento_idProcedimento','=','procedimentos.id')
-        ->join('clientes','consultas.Cliente_idCliente','=','cliente.id')
+        ->join('clientes','consultas.Cliente_idCliente','=','clientes.id')
         // ->join('clientes','clientes.Cliente_idCliente','=','clientes.id')
         ->select('consultas.*','procedimentos.*','clientes.*')->get();
 
