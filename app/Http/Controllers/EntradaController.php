4a4ce2c0c0bfc226c6cf3614c9b9cd5cf11ea32f
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-04-06 15:28:38 -0300

    Atualizando Listagem Controllers

--- app/Http/Controllers/EntradaController.php
+++ app/Http/Controllers/EntradaController.php
@@ -20,28 +20,37 @@ class EntradaController extends Controller
             'Estoque_idEstoque.integer' => 'Estoque não cadastrado em nosso sistema',
             'Colaborador_idColaborador.required' => 'Colaborador é um campo obrigatório',
             'Colaborador_idColaborador.integer' => 'Colaborador não cadastrado em nosso sistema',
+            'quantidade.required' => 'Quantidade é um campo obrogatório',
         ];
         $this->validate($request, [
             'Estoque_idEstoque' => 'required|integer',
             'Colaborador_idColaborador' => 'required|integer',
+            'quantidade' => 'required|integer',
         ],$messages);
         $entrada =  new Entrada();
         $entrada->Estoque_idEstoque = $request->Estoque_idEstoque;
         $entrada->Colaborador_idColaborador = $request->Colaborador_idColaborador;
+        $entrada->quantidade = $request->quantidade;
+
+        $dadoEstoque = Estoque::find($request->Estoque_idEstoque);
+        $valorFinal = $request->quantidade + $dadoEstoque->quantidade;
+
+        $dadoEstoque->update($valorFinal);
         $entrada->save();
         return response()->json('Entrada Com Sucesso!!');
     }
-    public function deletarEntrada($id)
-    {
-        $entrada = Entrada::find($id);
-        $del = $entrada->delete();
-        return response()->json('Entrada Deletado Com Sucesso!!');
-    }
-    public function atualizarEntrada(Request $request,$id)
-    {
-        $entrada = $request->all();
-        Entrada::find($id)->update($entrada);
-        return response()->json('Entrada Atualizado Com Sucesso!!');
+    // public function deletarEntrada($id)
+    // {
+    //     $entrada = Entrada::find($id);
+    //     $del = $entrada->delete();
+    //     return response()->json('Entrada Deletado Com Sucesso!!');
+    // }
+    // public function atualizarEntrada(Request $request,$id)
+    // {
+    //     $entrada = $request->all();
+    //     Entrada::find($id)->update($entrada);
+    //     return response()->json('Entrada Atualizado Com Sucesso!!');
 
-    }
+    // }
+    
 }
