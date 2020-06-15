74dfe2c7dd77a98eb74b570b09bdab3b7c3324ff
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-06-14 21:25:45 -0300

    Atualizando Controllers

--- routes/web.php
+++ routes/web.php
@@ -115,8 +115,9 @@ $router->post('/api/cadastrarUsuarios','UsuarioController@cadastrarUsuario');
 $router->get('/api/mostrarUmUsuario/{id}','UsuarioController@mostrarUmUsuario');
 $router->post('/api/atualizarUsuarios/{id}','UsuarioController@atualizarUsuario');
 $router->delete('/api/deletarUsuario/{id}','UsuarioController@deletar');
-$router->get('/api/testeLogin', 'ClienteController@teste');
+
 });
+$router->get('/api/testeLogin', 'ClienteController@teste');
 //Rota Login
 $router->post('/login','UsuarioController@usuarioLogin');
 $router->post('/info' ,'UsuarioController@mostrarUsuarioAutenticado');
