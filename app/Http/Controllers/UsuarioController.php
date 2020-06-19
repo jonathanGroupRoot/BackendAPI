221f21a59d9272239b17e5ee4204fec50d4eee03
jonathanGroupRoot <58049154+jonathanGroupRoot@users.noreply.github.com>
2020-06-18 21:32:52 -0300

    Atualizando Controllers

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -39,7 +39,7 @@ class UsuarioController extends Controller
             'password' => 'required|min:8:max:40',
         ],$messages);
         // if(! $token = $this->jwt->claims(['usuario' => $request->usuario])->attempt($request->only('usuario','password')))
-        if(! $token = $this->jwt->attempt($request->only('email','password'))
+        if(! $token = $this->jwt->attempt($request->only('email','password')))
         {
             return response()->json(['Usuário não encontrado'],404);
         }
